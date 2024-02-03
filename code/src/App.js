f764ff6ef59480fc77cada4701d5128a04607a5f
lhfirass <81319618+lhfirass@users.noreply.github.com>
2024-02-03 13:38:28 +0100

    commit: 03/02/2024 : 13:37
    + add vite library
    // actually it want work unless i create the project using it.
    * i will create a new project

--- code/src/App.js
+++ code/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="App__container">
+        <div className="App__logo">
+          
+        </div>
+        <div className="App_header">
+          <div className="App__element-x_default">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
